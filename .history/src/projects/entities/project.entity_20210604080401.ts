320a5ee4eb6c9e80960138717bcdc4643d81e7c4
Naruto Sam <sam.mamane@hotmail.fr>
2021-06-04 18:21:57 +0200

    Add procfile

---
+++ .history/src/projects/entities/project.entity_20210604080401.ts
@@ -0,0 +1,34 @@
+import {
+  BaseEntity,
+  BeforeInsert,
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  OneToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+  JoinTable,
+} from 'typeorm';
+import { User } from '../../users/entities/user.entity';
+
+@Entity()
+export class Project extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @Column()
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @OneToOne(() => User, (user: User) => user.projects)
+  @JoinTable()
+  user: User;
+}
