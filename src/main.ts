67bc4c14392dcfab2a9b2c73b3d706c25bd70c08
Naruto Sam <sam.mamane@hotmail.fr>
2021-06-04 19:00:15 +0200

    Process env

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await process.env.PORT || app.listen(3000);
+  await app.listen(process.env.PORT || 8080);;
 }
 bootstrap();
